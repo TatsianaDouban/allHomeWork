316ebf14621b7735a9e5cc521ff72918582e993e
tatsiana <Tatsiana3345260@gmail.com>
2023-02-03 09:59:10 +0300

    week20

---
+++ week21/htppAlisa.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("https://api.github.com/TatsianaDouban")
+    .then(response => response.json())
+    
+    .then(user => {
+        document.querySelector(".name").
+
+    })
+})
+
